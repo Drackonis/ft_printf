b93d9ff5e35bcf22b58ef1e0bc1c8ae1ba0b8f24
Drackonis <kergastel@hotmail.fr>
2019-05-20 16:30:09 +0200

    main

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: dieroyer <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/13 17:44:08 by dieroyer          #+#    #+#             */
-/*   Updated: 2019/05/20 16:12:00 by dieroyer         ###   ########.fr       */
+/*   Updated: 2019/05/20 16:21:20 by rkergast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,9 @@
 int		main(int ac, char **av)
 {	t_printf p;
 	float ff =  123456789.123123;
-	printf("PRINTF OG = %f\n", FLT_MAX);
-	printf("PRINT OG2 = %f\n", ff);
-	ft_printf("MY PRINTF = %f\n", ff);
+	//printf("PRINTF OG = %f\n", FLT_MAX);
+	printf(av[1], ff);
+	printf("\n");
+	ft_printf(av[1], ff);
 }
 
