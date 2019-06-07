a561cda120c35f23140e1b7a170462e61635269c
dirabek <dieroyer@student.42.fr>
2019-06-07 12:42:10 +0200

    s et c

--- ft_printf_s.c
+++ ft_printf_s.c
@@ -6,7 +6,7 @@
 /*   By: dieroyer <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 16:07:54 by dieroyer          #+#    #+#             */
-/*   Updated: 2019/06/05 13:23:49 by dieroyer         ###   ########.fr       */
+/*   Updated: 2019/06/07 12:36:29 by dieroyer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ t_printf	char_conv(t_printf p)
 	void	*v;
 	int		i;
 
+	p = initialize(p);
 	p = is_modifier(p);
 	p = ft_flag_modifier(p);
 	i = p.i + ft_strlen(p.conv) + 1;
@@ -43,6 +44,7 @@ t_printf	string_conv(t_printf p)
 {
 	int		i;
 
+	p = initialize(p);
 	p = is_modifier(p);
 	i = p.i + ft_strlen(p.conv) + 1;
 	if (p.format[i] == 's' && p.format[i - 1] != 'l')
@@ -90,7 +92,7 @@ t_printf	str_bonus(t_printf p)
 
 	p = is_modifier(p);
 	i = p.i + ft_strlen(p.conv) + 1;
-	if ((p.format[i]) == 'S' || (p.format[p.i] == 's' 
+	if ((p.format[i]) == 'S' || (p.format[p.i] == 's'
 				&& p.format[i - 1] == 'l'))
 	{
 		v = va_arg(p.arg, wchar_t*);
